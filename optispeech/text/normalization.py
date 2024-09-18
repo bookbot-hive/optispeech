2e9f3e2d8514f7b8bc5ba287d30c0f75e493b5a0
w11wo <wilsonwong961@gmail.com>
2024-09-18 10:02:08 +1000

    Added en-Multi-Althaf gruut Recipe

--- optispeech/text/normalization.py
+++ optispeech/text/normalization.py
@@ -9,14 +9,10 @@ def preprocess_text(text: str, language: str = None, *, normalize: bool = False)
     if normalize:
         text = unicodedata.normalize(UNICODE_NORM_FORM, text)
 
-    # remove spaces between quotes
-    text = re.sub(r'"(\s)(.*?)(\s)"', r'"\2"', text)
-    # remove spaces before punctuation
-    text = re.sub(r"\s([.,!?;])", r"\1", text)
     # collapse consecutive dots
     text = re.sub(r"\.{2,}", ".", text)
     # handle consecutive punctuations
-    text = re.sub(r"(\.\?)|(\?\!)|(\!\?)|(\?.)", "?", text)
+    text = re.sub(r"(\.\?)|(\?\!)|(\!\?)|(\?\.)", "?", text)
     text = re.sub(r"(\!\.)|(\!\,)", "!", text)
     text = collapse_whitespace(text)
     return text
